a6233fd9f14eaf1c098c5ff174e9abcaf29dc537
valentina <valentinacalvod1@upb.edu>
2025-11-28 09:27:59 -0400

    Base Structure for Modules Dev

---
+++ App.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { KeyboardProvider } from "react-native-keyboard-controller";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import ErrorBoundary from "@/src/core/components/ErrorBoundary";
+import RootNavigator from "@/src/core/navigation/RootNavigator";
+
+export default function App() {
+  return (
+    <ErrorBoundary>
+      <SafeAreaProvider>
+        <GestureHandlerRootView style={styles.root}>
+          <KeyboardProvider>
+            <NavigationContainer>
+              <RootNavigator />
+            </NavigationContainer>
+            <StatusBar style="light" />
+          </KeyboardProvider>
+        </GestureHandlerRootView>
+      </SafeAreaProvider>
+    </ErrorBoundary>
+  );
+}
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+});
